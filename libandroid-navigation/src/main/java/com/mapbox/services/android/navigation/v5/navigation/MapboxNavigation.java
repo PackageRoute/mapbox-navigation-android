6f00b7b40c09b248d91e4811b080ac396685ff54
abhishek1508 <abhishek.kejriwal@mapbox.com>
2019-09-30 13:01:26 -0700

    change file names

--- libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigation.java
+++ libandroid-navigation/src/main/java/com/mapbox/services/android/navigation/v5/navigation/MapboxNavigation.java
@@ -392,7 +392,7 @@ public class MapboxNavigation implements ServiceConnection {
       MapboxMetricsReporter.disable();
       navigationService.stopSelf();
       navigationEventDispatcher.onNavigationEvent(false);
-      AccountsManagerImpl.getInstance(applicationContext).endNavigation();
+      AccountsManager.getInstance(applicationContext).endNavigation();
     }
   }
 

6f6ff66055cef83e72f11735413577ca5f279533
Yury Kanetski <yury.kanetski@mapbox.com>
2019-11-05 18:25:50 +0300

    Navigator make public

--- libdirections-onboard/src/main/java/com/mapbox/navigation/route/onboard/MapboxOnboardRouter.kt
+++ libdirections-onboard/src/main/java/com/mapbox/navigation/route/onboard/MapboxOnboardRouter.kt
@@ -6,6 +6,7 @@ import com.mapbox.geojson.Point
 import com.mapbox.navigation.base.route.Router
 import com.mapbox.navigation.navigator.MapboxNativeNavigator
 import com.mapbox.navigation.navigator.MapboxNativeNavigatorImpl
+import com.mapbox.navigation.route.onboard.task.OfflineRouteRetrievalTask
 import java.io.File
 
 @MapboxNavigationModule(MapboxNavigationModuleType.OnboardRouter, skipConfiguration = true)
@@ -16,10 +17,9 @@ class MapboxOnboardRouter(private val navigator: MapboxNativeNavigator) : Router
         private const val TILE_PATH_NAME = "tiles"
     }
 
-    // private val offlineNavigator: OfflineNavigator
     private val tilePath: String
     private val offlineTileVersions: OfflineTileVersions
-    private val navigator: MapboxNativeNavigator
+    private val navigatorNative: MapboxNativeNavigator
 
     /**
      * Creates an offline router which uses the specified offline path for storing and retrieving
@@ -35,7 +35,7 @@ class MapboxOnboardRouter(private val navigator: MapboxNativeNavigator) : Router
 
         this.tilePath = tileDir.absolutePath
         offlineTileVersions = OfflineTileVersions()
-        this.navigator = MapboxNativeNavigatorImpl
+        this.navigatorNative = MapboxNativeNavigatorImpl
     }
 
     // Package private for testing purposes
@@ -46,7 +46,7 @@ class MapboxOnboardRouter(private val navigator: MapboxNativeNavigator) : Router
     ) {
         this.tilePath = tilePath
         this.offlineTileVersions = offlineTileVersions
-        this.navigator = navigator
+        this.navigatorNative = navigator
     }
 
     override fun getRoute(

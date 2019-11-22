e53b20c96b1d94483d5e97e275ca6a15c377d7db
Raman Lebiadzinski <raman.lebiadzinski@mapbox.com>
2019-11-22 19:03:27 +0300

    Fix tests. Remove unneeded RouteRetrieveCallback, roll back OffboardRouter changes.

--- libdirections-offboard/src/main/java/com/mapbox/navigation/route/offboard/MapboxOffboardRouter.kt
+++ libdirections-offboard/src/main/java/com/mapbox/navigation/route/offboard/MapboxOffboardRouter.kt
@@ -3,22 +3,50 @@ package com.mapbox.navigation.route.offboard
 import android.content.Context
 import com.mapbox.annotation.navigation.module.MapboxNavigationModule
 import com.mapbox.annotation.navigation.module.MapboxNavigationModuleType
+import com.mapbox.api.directions.v5.models.DirectionsResponse
 import com.mapbox.geojson.Point
 import com.mapbox.navigation.base.route.Router
-import com.mapbox.navigation.route.offboard.callback.RouteRetrieveCallback
+import com.mapbox.navigation.route.offboard.extension.mapToRoute
 import com.mapbox.navigation.route.offboard.router.NavigationRoute
+import com.mapbox.navigation.utils.exceptions.NavigationException
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 @MapboxNavigationModule(MapboxNavigationModuleType.OffboardRouter, skipConfiguration = true)
-class MapboxOffboardRouter(
-    private val context: Context,
-    private val mapboxToken: String
-) : Router {
+class MapboxOffboardRouter : Router {
 
+    companion object {
+        const val ERROR_FETCHING_ROUTE = "Error fetching route"
+    }
+
+    private val context: Context
+    private val mapboxToken: String
+    private val routeBuilderProvider: () -> NavigationRoute.Builder
     private var navigationRoute: NavigationRoute? = null
-    // For testing only
-    private var isNeedToBuildNavigationRoute = true
-    // For testing only
-    private lateinit var routeRetrieveCallback: RouteRetrieveCallback
+
+    constructor(
+        context: Context,
+        mapboxToken: String
+    ) : this(
+        context,
+        mapboxToken,
+        {
+            NavigationRoute
+                .builder(context)
+                .accessToken(mapboxToken)
+        }
+    )
+
+    internal constructor(
+        context: Context,
+        mapboxToken: String,
+        routeBuilderProvider: () -> NavigationRoute.Builder
+    ) {
+        this.context = context
+        this.mapboxToken = mapboxToken
+        this.routeBuilderProvider = routeBuilderProvider
+    }
 
     override fun getRoute(
         origin: Point,
@@ -26,34 +54,34 @@ class MapboxOffboardRouter(
         destination: Point,
         callback: Router.Callback
     ) {
-        if (isNeedToBuildNavigationRoute) {
-            val builder = NavigationRoute
-                .builder(context)
-                .accessToken(mapboxToken)
-                .origin(origin)
-                .destination(destination)
-            waypoints.forEach { builder.addWaypoint(it) }
-            navigationRoute = builder.build()
-        }
-        if (!::routeRetrieveCallback.isInitialized) {
-            routeRetrieveCallback = RouteRetrieveCallback(callback)
-        }
-        navigationRoute?.getRoute(routeRetrieveCallback)
+        val builder = routeBuilderProvider.invoke()
+        builder
+            .origin(origin)
+            .destination(destination)
+        waypoints.forEach { builder.addWaypoint(it) }
+        navigationRoute = builder.build()
+        navigationRoute?.getRoute(object : Callback<DirectionsResponse> {
+
+            override fun onResponse(
+                call: Call<DirectionsResponse>,
+                response: Response<DirectionsResponse>
+            ) {
+                val routes = response.body()?.routes()
+                if (response.isSuccessful && !routes.isNullOrEmpty()) {
+                    callback.onRouteReady(routes.map { it.mapToRoute() })
+                } else {
+                    callback.onFailure(NavigationException(ERROR_FETCHING_ROUTE))
+                }
+            }
+
+            override fun onFailure(call: Call<DirectionsResponse>, t: Throwable) {
+                callback.onFailure(t)
+            }
+        })
     }
 
     override fun cancel() {
         navigationRoute?.cancelCall()
         navigationRoute = null
-    }
-
-    // For testing only
-    internal fun setNavigationRoute(navigationRoute: NavigationRoute) {
-        this.navigationRoute = navigationRoute
-        isNeedToBuildNavigationRoute = false
-    }
-
-    // For testing only
-    internal fun setRouteRetrieveCallback(routeRetrieveCallback: RouteRetrieveCallback) {
-        this.routeRetrieveCallback = routeRetrieveCallback
     }
 }
